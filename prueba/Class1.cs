7624c907decda14626ea34e5891f84d46eb3a4ca
Cupsuki001 <124387400+Cupsuki001@users.noreply.github.com>
2024-02-02 11:38:08 -0600

    Add files via upload

---
+++ prueba/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba
+{
+    internal class Class1
+    {
+        double  v1 , e1 , tt1 , tt2;
+
+        public Class1(){}
+
+        public Class1(double v1) 
+        {
+         this.v1 = v1;
+        }
+        public void opc() 
+        {
+            Console.WriteLine("Divisas");
+            Console.WriteLine("1.op Vender");
+            Console.WriteLine("2.op comprar");
+            Console.WriteLine("3.op Salir");
+        }
+
+        public void opc2() 
+        {
+            Console.WriteLine("Vender");
+            Console.WriteLine("1.op Dolares");
+            Console.WriteLine("2.op Euros");
+        }
+
+        public void opc3()
+        {
+            Console.WriteLine("Comprar");
+            Console.WriteLine("1.op Dolares");
+            Console.WriteLine("2.op Euros");
+        }
+
+        public double venderD(double v1) 
+        {
+            return v1 * 36.10;
+            Console.WriteLine("Cordobas " + v1); 
+        }
+
+        public double venderE(double e1) 
+        {
+            tt1 = e1 * 1.0350;
+            tt2 = tt1 * 36.10;
+            return  tt2;
+        }
+
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.Write(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("////////////////////////////////////");
+                    Console.WriteLine("No se aceptan letras", error.Message);
+                    Console.WriteLine("////////////////////////////////////");
+                    opc();
+                }
+            } while (v == 0);
+            return num;
+        }
+
+
+    }
+    
+}
---
+++ prueba/Program.cs
@@ -0,0 +1,109 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using prueba;
+
+public class p 
+{
+    public static void Main(string [] args) 
+    {
+      Class1 r = new Class1();
+
+        int op , op2 ;
+        double cd, td;
+
+        do 
+        {
+
+            do
+            {
+                Console.WriteLine("//////////////////////");
+                r.opc();
+                op = r.validar("Seleccione una opcion: ");
+                Console.WriteLine("//////////////////////");
+
+            } while (op < 0 || op > 3);
+
+            switch (op) 
+            {
+                case 1: 
+                    {
+                        do
+                        {
+                            Console.WriteLine("//////////////////////");
+                            r.opc2();
+                            op2 = r.validar("Seleccione una opcion: ");
+                            Console.WriteLine("//////////////////////");
+
+                        } while (op < 0 || op > 3);
+
+                        switch (op2) 
+                        {
+                          case 1: 
+                                {
+                                    do
+                                    {
+                                        Console.WriteLine("DOLARES");
+                                        cd = r.validar("Ingrese la cantidad a vender: ");
+                                    } while (cd < 0 || cd > 50);
+                                    td = r.venderD(cd);
+                                    Console.WriteLine("Dolares ingresados: "+ cd);
+                                    Console.WriteLine("la cantidad en cordobas es " + td);
+                                    
+                                  
+                                }
+                                break;
+
+                            case 2:
+                                {
+                                    do
+                                    {
+                                        Console.WriteLine("Euros");
+                                        cd = r.validar("Ingrese la cantidad a vender: ");
+                                    } while (cd < 0 || cd > 50);
+                                    td = r.venderE(cd);
+                                    Console.WriteLine("Euros ingresados: " + cd);
+                                    Console.WriteLine("la cantidad en cordobas es " + td);
+
+
+                                }
+                                break;
+                        }
+                    }
+                    break;
+                    case 2: 
+                    {
+                        do
+                        {
+                            Console.WriteLine("//////////////////////");
+                            r.opc3();
+                            op2 = r.validar("Seleccione una opcion: ");
+                            Console.WriteLine("//////////////////////");
+
+                        } while (op2 < 0 || op2 > 3);
+
+                        switch (op2)
+                        {
+                            case 1:
+                                {
+                                    do
+                                    {
+                                        Console.WriteLine("DOLARES");
+                                        cd = r.validar("Ingrese la cantidad a Comprar: ");
+                                    } while (cd < 0);
+                                    td = r.venderD(cd);
+                                    Console.WriteLine("Dolares ingresados: " + cd);
+                                    Console.WriteLine("la cantidad en cordobas es " + td);
+
+
+                                }
+                                break;
+                        }
+                    }
+                    break;
+            }
+
+
+
+        } while (true);
+    }
+}
